9c82948311b22e884db8d3958f3f1d1077ad80a8
Leonel Veliz <57767278+leonelvelizaponte@users.noreply.github.com>
2019-11-18 19:11:05 -0300

    se actualizan config

--- tools/visualstudiocode/server/servernode/config.js
+++ tools/visualstudiocode/server/servernode/config.js
@@ -2,8 +2,8 @@
 module.exports = {
     port: 3001,
     serverdata: 'https://hn.algolia.com/api/v1/search_by_date?query=nodejs',
-    mongouser: 'atlasAdmin',
-    mongopass: 'atlasAdmin',
-    mongostring: 'cluster0-8gtfa.mongodb.net/test?retryWrites=true&w=majority', //Cuando creas el string de conexion de mongoatlas, parte del string es @calgo-18jqbr.mongodb.net/test... se debe colocar todo luego del @
-    mongoclustername: 'Cluster0',
+    mongouser: 'mongouser',
+    mongopass: 'mongopass',
+    mongostring: 'clusterNuevo123.mongodb.net/teste?retryWritese=true&w=majeority', //Cuando creas el string de conexion de mongoatlas, parte del string es @calgo-18jqbr.mongodb.net/test... se debe colocar todo luego del @
+    mongoclustername: 'mongoclustername',
 }
